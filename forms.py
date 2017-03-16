d3fff2ffe6711d83c7e285f657227c9254df81ea
mr.waffles <mr.waffles@gmail.com>
2017-03-16 16:22:35 -0400

    Made some trash placeholder HTML

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import ContractorClass, RevenueBand, MoldHazardGroup, Limit, Deductible, Aggregate, Nose
 
-class ContractorClassForm(forms.ModelForm):
-  class Meta:
-    model = ContractorClass
+class ContractorClassForm(forms.Form):
+  iso_code = forms.IntegerField(label = "ISO Code")
+  class_revenue = forms.IntegerField(label = "Revenue")
+  mold_hazard_group = forms.CharField(label = "Mold Hazard Group")
     
--- views.py
+++ views.py
@@ -7,34 +7,59 @@ from django.forms import inlineformset_factory, formset_factory
 from django.http import HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from decimal import Decimal
+from .forms import ContractorClassForm
 from .models import ContractorClass, RevenueBand, MoldHazardGroup, Limit, Deductible, Aggregate, Nose
 import json
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.utils.decorators import method_decorator
 
 # Create your views here.
-class ContractorClassBaseRate:
-  def __init__(self, iso_code, class_revenue, mold_hazard_group):
+class ContractorBaseRate:
+  def __init__(self, iso_code, revenue, mold_hazard_group):
     iso_code = int(iso_code)
-    class_revenue = int(class_revenue)
+    revenue = int(revenue)
     contractor_class = ContractorClass.objects.get(iso_code__iexact = iso_code)
     self.iso_code = contractor_class.iso_code
-    self.premium_ex_mold = contractor_class.get_premium_ex_mold(class_revenue)
-    self.mold_premium = contractor_class.get_mold_premium(class_revenue, mold_hazard_group)
-    self.premium = contractor_class.get_total_premium(class_revenue, mold_hazard_group)
+    self.premium_ex_mold = contractor_class.get_premium_ex_mold(revenue)
+    self.mold_premium = contractor_class.get_mold_premium(revenue, mold_hazard_group)
+    self.premium = contractor_class.get_total_premium(revenue, mold_hazard_group)
   
   def __str__(self):
     return "ISO Code: %s Premium: %s" % (self.iso_code, self.premium)
+
+class Submission:  
+  def __init__(self, submission_data, type):     
+    self.type = type
+    self.contractor_classes = []
+    contractor_array = submission_data[type]['contractor_classes']
+    for contractor_data in contractor_array:
+      c = ContractorBaseRate(**contractor_data)
+      self.contractor_classes.append(c)
+    self.manual_rate = submission_data[type]['manual_rate'] 
   
 class Index(View):    
   def get(self, request, *args, **kwargs):
-    contractor_classes = ContractorClass.objects.all()
-    context = {"contractor_classes" : contractor_classes}
-    return render(request, 'envirorater/home.html', context)  
+    contractor_classes = ContractorClass.objects.all()    
+    context = {"contractor_classes" : contractor_classes}    
+    return render(request, 'envirorater/home.html', context)
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ContractorBaseRateAPI(View):    
-#   Provide a GET request with iso_code, class_revenue, and mold_hazard_group
+#   Provide a GET request with iso_code, revenue, and mold_hazard_group
 #   Returns JsonResponse with contractor class key values
+#   Note need to use REST Framework to surprass the Cross Origin Problem
     
     def get(self, request, *args, **kwargs):
       request_dictionary = request.GET.dict()
-      contractor_class = ContractorClassBaseRate(**request_dictionary)
+      contractor_class = ContractorBaseRate(**request_dictionary)
       return JsonResponse(contractor_class.__dict__)
+    
+    def post(self, request, *args, **kwargs):
+      submission_data = json.loads(request.body)
+      submission = Submission(submission_data, 'cpl_submission')
+      print(submission)
+      return JsonResponse(submission, safe=False)
+        
+      
+      
+ 
